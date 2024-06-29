b2719ee80cc6611456fc7f5f774dc578cf5b8c76
wabjay <jefferyjohn230@gmail.com>
2024-06-29 03:35:11 +0100

    first commit

--- src/js/component.js
+++ src/js/component.js
@@ -12,37 +12,39 @@
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
-            // console.log(data)
-        });
+        })
+        .catch(error => console.error('Error loading header:', error));
+        
+  // Activate the link based on the current URL
+  activateLinkByURL();
 });
 
 
-  // JavaScript to load footer content
-  document.addEventListener("DOMContentLoaded", function() {
-    fetch("./src/constants/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-placeholder").innerHTML = data;
-        });
+document.addEventListener("DOMContentLoaded", function() {
+  // Load footer content
+  fetch("./src/constants/footer.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("footer-placeholder").innerHTML = data;
+      })
+      .catch(error => console.error('Error loading footer:', error));
+
 });
 
-// update the active link
 function activateLinkByURL() {
   const currentPageURL = new URL(window.location.href).pathname; // Get pathname (without parameters)
 
   // Find all header links
-  const headerLinks = document.querySelectorAll(".link"); // Replace ".header-link" with your actual selector
+  const headerLinks = document.querySelectorAll(".link"); // Replace ".link" with your actual selector
 
   // Loop through header links
   headerLinks.forEach(link => {
-    const linkURL = new URL(link.getAttribute("href")).pathname; // Get pathname from link href
-console.log(currentPageURL, linkURL)
-    if (currentPageURL === linkURL) {
-      link.classList.add("bg-grey-1"); // Add "active" class to the matching link
-    } else {
-      link.classList.remove("bg-grey-1"); // Remove "active" class from non-matching links (optional)
-    }
+      const linkURL = new URL(link.getAttribute("href"), window.location.origin).pathname; // Get pathname from link href
+      console.log(currentPageURL, linkURL);
+      if (currentPageURL === linkURL) {
+          link.classList.add("bg-grey-1"); // Add "active" class to the matching link
+      } else {
+          link.classList.remove("bg-grey-1"); // Remove "active" class from non-matching links (optional)
+      }
   });
 }
-
-activateLinkByURL();
