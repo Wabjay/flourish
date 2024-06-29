f901023699743cf11dc5f7137136619648c30200
wabjay <jefferyjohn230@gmail.com>
2024-06-29 03:57:10 +0100

    first commit

--- src/js/index-component.js
+++ src/js/index-component.js
@@ -2,17 +2,17 @@
 const sections = ['faq', 'hero', 'growth', 'build', 'statement', 'partners', 'waitlist'];
 
 document.addEventListener("DOMContentLoaded", function() {
-  const loadSectionPromises = sections.map(section => {
-      return fetch(`./src/constants/index/${section}.html`)
-          .then(response => response.text())
-          .then(data => {
-              const placeholder = document.getElementById(`${section}-placeholder`);
-              if (placeholder) {
-                  placeholder.innerHTML = data;
-              }
-          })
-          .catch(error => console.error('Error loading section:', section, error));
-  });
+//   const loadSectionPromises = sections.map(section => {
+//       return fetch(`./src/constants/index/${section}.html`)
+//           .then(response => response.text())
+//           .then(data => {
+//               const placeholder = document.getElementById(`${section}-placeholder`);
+//               if (placeholder) {
+//                   placeholder.innerHTML = data;
+//               }
+//           })
+//           .catch(error => console.error('Error loading section:', section, error));
+//   });
 
   // Once all sections are loaded, add event listeners to .question elements
   Promise.all(loadSectionPromises).then(() => {
