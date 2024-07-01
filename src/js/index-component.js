eab242fdde52688ef1ec669bad7b3584186a5f04
wabjay <jefferyjohn230@gmail.com>
2024-07-01 14:43:20 +0100

    Preview

--- src/js/index-component.js
+++ src/js/index-component.js
@@ -15,22 +15,40 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Once all sections are loaded, add event listeners to .question elements
-  Promise.all(loadSectionPromises).then(() => {
-      document.querySelectorAll('.question').forEach(item => {
-          item.addEventListener('click', () => {
-              const answer = item.nextElementSibling;
-              const arrow = item.querySelector('.arrow');
-              if (answer.style.display === 'block') {
-                  answer.style.display = 'none';
-                  arrow.classList.remove('bggold');
-                  arrow.innerHTML = "-";
+//   Promise.all(loadSectionPromises).then(() => {
+//       document.querySelectorAll('.question').forEach(item => {
+//           item.addEventListener('click', () => {
+//               const answer = item.nextElementSibling;
+//               const arrow = item.querySelector('.arrow');
+//               if (answer.style.display === 'block') {
+//                 //   answer.style.display = 'none';
+//                   arrow.classList.remove('bggold');
+//                   arrow.innerHTML = "+";   
+//                   answer.classList.toggle('hidden');
+//                   answer.style.maxHeight = content.classList.contains('hidden') ? '0' : `${content.scrollHeight}px`;
+//               } else {
+//                   answer.style.display = 'block';
+//                   arrow.classList.add('bggold');
+//                   arrow.innerHTML = "-";
+//               }
+//           });
+//       });
+//   });
+// });
 
-              } else {
-                  answer.style.display = 'block';
-                  arrow.classList.add('bggold');
-                  arrow.innerHTML = "+";
-              }
-          });
-      });
-  });
+
+
+  // Once all sections are loaded, add event listeners to .question elements
+  Promise.all(loadSectionPromises).then(() => {
+    document.querySelectorAll('.question').forEach(item => {
+        item.addEventListener('click', () => {
+            const answer = item.nextElementSibling;
+            const arrow = item.querySelector('.arrow');
+                arrow.classList.toggle('bggold');
+                answer.classList.contains('hidden') ? arrow.innerHTML = "-" : arrow.innerHTML = "+";   
+                answer.classList.toggle('hidden');
+                answer.style.maxHeight = answer.classList.contains('hidden') ? '0' : `${answer.scrollHeight}px`;
+        });
+    });
+});
 });
